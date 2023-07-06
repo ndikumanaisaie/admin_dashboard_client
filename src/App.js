2429eabc9b93423e0f14702299c1b36db292d2a7
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-07-06 11:39:34 +0200

    Add the layout and dashboard components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
 import './App.css';
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<Navigate to="/dashboard" replace />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Route>
+            </Routes>
+          </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
